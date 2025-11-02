729e196e6cbd2a44f2c0988c287ff8eee527aea4
anu235shka <anushka23.54.sharma@gmail.com>
2025-11-03 03:53:06 +0530

    build-server

--- server/src/validators/entrySchemas.ts
+++ server/src/validators/entrySchemas.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const entryCreateSchema = z.object({
   title: z.string().min(1, 'Title is required'),
-  type: z.enum(['MOVIE', 'TV_SHOW'], { errorMap: () => ({ message: 'Type must be MOVIE or TV_SHOW' }) }),
+  type: z.enum(['MOVIE', 'TV_SHOW'], { message: 'Type must be MOVIE or TV_SHOW' }),
   director: z.string().optional().transform(v => v === '' ? undefined : v),
   budget: z.union([z.number(), z.string().transform(v => v === '' ? undefined : Number(v))]).optional(),
   location: z.string().optional().transform(v => v === '' ? undefined : v),
